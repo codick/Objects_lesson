0a4bb14e0d7a795482fc7c080c277fbf2fc0a3b7
codick <114742863+codick@users.noreply.github.com>
2022-11-07 11:53:41 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,143 @@
+// Задание 1
+
+// let arr = ['Привет,',' мир','!']
+// let str = ''
+// for (item of arr){
+//     str += item
+// }
+// alert(str)
+
+// Задание 2
+
+// let arr = ['Привет, ', 'мир', '!']
+// let text = ''
+// for (item of arr){
+//     text += item
+// }
+// alert(text)
+
+// Задание 3
+
+// let arr = ['Привет', ' мир', '!'];
+// arr[0] = 'Пока,';
+// let str = '';
+// for (item of arr){
+//     str += item;
+// }
+// alert(str);
+
+// Задание 4
+
+// let money = {
+//     Nikolay : 300,
+//     Petr : 400
+// }
+// alert(`Заработная плата Коли = ${money.Nikolay} долларов в месяц`);
+// alert(`Заработная плата Пети = ${money.Petr} долларов в месяц`);
+
+// Задание 5
+
+// var arr = {
+//     'ru' : ['голубой', 'красный', 'зеленый'],
+//     'en' : ['blue', 'red', 'green']
+// }
+
+// alert(arr.ru[1]);
+
+// Задание 6
+
+// let arr = ['a', 'b', 'c'];
+// alert(arr);
+
+// Задание 7
+
+// let arr = ['a', 'b', 'c'];
+// alert(arr[0]);
+// alert(arr[1]);
+// alert(arr[2]);
+
+// Задание 8
+
+// let arr = ['a', 'b', 'c', 'd'];
+// alert(`${arr[0]} + ${arr[1]} , ${arr[2]} + ${arr[3]}`)
+
+// Задание 9
+
+// let arr = [2,5,3,9];
+// let result = 0;
+// result = (arr[0] * arr[1]) + (arr[2] * arr[3])
+// alert(result)
+
+// Задание 10
+
+// var obj = {a: 1, b:2, c:3};
+// alert(obj.c)
+// alert(obj['c'])
+
+// Задание 11
+
+// var obj = {'Коля': 1000, 'Вася':500, 'Петя': 200};
+// alert(`Заработная плата Коли = ${obj['Коля']} долларов в месяц`);
+// alert(`Заработная плата Пети = ${obj['Петя']} долларов в месяц`);
+
+// Задание 12
+
+// var obj = {
+//     'First' : 'Понедельник',
+//     'Second' : 'Вторник',
+//     'Third' : 'Среда',
+//     'Fourth' : 'Четверг',
+//     'Fifth' : 'Пятница',
+//     'Sixth' : 'Суббота',
+//     'Seventh' : 'Воскресенье'
+// }
+
+// alert(obj['First']);
+
+// Задание 13
+
+// var obj = {
+//     '1' : 'Понедельник',
+//     '2' : 'Вторник',
+//     '3' : 'Среда',
+//     '4' : 'Четверг',
+//     '5' : 'Пятница',
+//     '6' : 'Суббота',
+//     '7' : 'Воскресенье'
+// }
+
+// let day = 3;
+
+// for (item in obj){
+//     if (item == day){
+//         alert(obj[item])
+//     }
+// }
+
+
+// Задание 14
+
+// let arr = [[1, 2, 3], [4, 5, 6], [7,8,9]];
+// alert(arr[1][0]);
+
+// Задание 15
+
+// let arr = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'};
+// alert(arr.js[0])
+
+// Задание 16
+
+// let arr = {'ru' : ['Понедельник', 'Вт', 'Ср', 'Чт', 'Пят', 'Суб', 'Вскр'], 'en': ['Mond', 'Tuesd', 'Wednesday', 'Thirsd', 'Frid', 'Str', 'Snd']};
+
+// alert(arr.ru[0]);
+// alert(arr.en[2]);
+
+// let lang = 'ru';
+// let day = 2;
+
+// let arr = {'ru' : ['Понедельник', 'Вт', 'Ср', 'Чт', 'Пят', 'Суб', 'Вскр'], 'en': ['Mond', 'Tuesd', 'Wednesday', 'Thirsd', 'Frid', 'Str', 'Snd']};
+
+// alert(arr[lang][day]);
+
+
+
